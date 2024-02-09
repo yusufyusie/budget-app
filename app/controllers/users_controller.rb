d85796de2aab94752808d4b4c934a3e88265e691
yusufyusie <yesuf023@gmail.com>
2024-02-09 06:40:06 +0300

    Add actions to the UsersController to allow users to view and edit their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :email)
+  end
 end
