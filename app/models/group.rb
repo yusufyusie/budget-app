5a58a7abf39f4cc44cf816dae5581562fcbafc08
yusufyusie <yesuf023@gmail.com>
2024-02-09 20:28:16 +0300

    Update models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ApplicationRecord
-  belongs_to :user
   has_many :group_purchases, dependent: :destroy
   has_many :purchases, through: :group_purchases
+
+  belongs_to :user
 
   validates :name, presence: true, length: { maximum: 36 },
                    uniqueness: { scope: :user, message: 'user already has this group' }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_09_113426) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_09_170231) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_09_113426) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -62,10 +61,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_09_113426) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "full_name"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "full_name"
     t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
