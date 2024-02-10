b0b14ade137fe4462742e6fdfe51155c92533dc2
yusufyusie <yesuf023@gmail.com>
2024-02-10 14:59:07 +0300

    Permit additional parameters (in this case, :name) for Devise in your Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   def after_sign_in_path_for(_resource)
